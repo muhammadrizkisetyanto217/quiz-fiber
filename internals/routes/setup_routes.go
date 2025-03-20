de3df295affffb828b18e65d3ec0310c18a9f5ed
muhammadrizkisetyanto <muhammadrizkisetyanto217@gmail.com>
2025-03-20 12:00:46 +0700

    category (2a)

--- internals/routes/setup_routes.go
+++ internals/routes/setup_routes.go
@@ -4,6 +4,9 @@ import (
 	// Add this line.
 	userRoute "quiz-fiber/internals/features/user/user/route"
 	authRoute "quiz-fiber/internals/features/user/auth/route"
+	difficultyRoute "quiz-fiber/internals/features/category/difficulty/route"
+	categoryRoute "quiz-fiber/internals/features/category/category/route"
+	subcategoryRoute "quiz-fiber/internals/features/category/subcategory/route"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -18,5 +21,10 @@ func SetupRoutes(app *fiber.App, db *gorm.DB) {
 
 	userRoute.UserRoutes(app, db)
 	authRoute.AuthRoutes(app, db)
+
+	//* Category
+	difficultyRoute.CategoryRoutes(app, db)
+	categoryRoute.CategoryRoutes(app, db)
+	subcategoryRoute.CategoryRoutes(app, db)
 
 }
